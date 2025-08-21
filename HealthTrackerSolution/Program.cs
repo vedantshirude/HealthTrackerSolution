7bcd8b2bbfb71b4905e011b57da458a6734d2d05
vedant <vedantshirude7@gmail.com>
2025-08-21 21:40:11 +0530

    Updated program.cs file

--- HealthTrackerSolution/Program.cs
+++ HealthTrackerSolution/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,9 +21,32 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Add services to the container.
-builder.Services.AddDbContext<dataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Get connection string from config
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Check if Render provides DATABASE_URL (Postgres)
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    // Render gives DATABASE_URL in the format:
+    // postgres://username:password@host:port/dbname
+    var dbUri = new Uri(databaseUrl);
+    var userInfo = dbUri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={dbUri.Host};Port={dbUri.Port};Database={dbUri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+
+    // Use Postgres when running on Render
+    builder.Services.AddDbContext<dataContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    // Default to SQL Server locally
+    builder.Services.AddDbContext<dataContext>(options =>
+        options.UseSqlServer(connectionString));
+}
+
 builder.Services.AddScoped<IUser, UserRepository>();
 
 var app = builder.Build();
